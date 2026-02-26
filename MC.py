21ba863f9b6af2aa7e0fdcc5aafa03164e4a8c7d
ploy250946-creator <ploy250946@gmail.com>
2026-02-26 16:15:26 +0700

    Update MC.py

--- MC.py
+++ MC.py
@@ -1,98 +1,147 @@
 # -*- coding: utf-8 -*-
+
 """
+
 Created on Thu Feb 19 15:58:18 2026
 
 @author: BusRmutt
-"""
 
+"""
+ 
 import pickle
+
 from streamlit_option_menu import option_menu
+
 import streamlit as st
+ 
+# Load Models
 
 used_car_model = pickle.load(open('Used_cars_model.sav','rb'))
+
 riding_model = pickle.load(open('RidingMowers_model.sav','rb'))
 
+bmi_model = pickle.load(open('bmi_model.sav','rb'))
+ 
+# Mapping Data
 
+fuel_map = {'Diesel': 0, 'Electric': 1, 'Petrol': 2}
 
-fuel_map = {
-    'Diesel': 0,
-    'Electric': 1,
-    'Petrol': 2
+engine_map = {'800': 0, '1000': 1, '1200': 2, '1500': 3, '1800': 4, '2000': 5, '2500': 6, '3000': 7, '4000': 8, '5000': 9}
+
+brand_map = {'BMW': 0, 'Chevrolet': 1, 'Ford': 2, 'Honda': 3, 'Hyundai': 4, 'Kia': 5, 'Nissan': 6, 'Tesla': 7, 'Toyota': 8, 'Volkswagen': 9}
+
+transmission_map = {'Automatic': 0, 'Manual': 1}
+
+gender_map = {'Male': 0, 'Female': 1}
+ 
+# BMI Result Mapping (อ้างอิงตาม Index ที่คุณระบุ)
+
+bmi_index_details = {
+
+    0: "Extremely Weak (ผอมมาก)",
+
+    1: "Weak (ผอม/น้ำหนักน้อย)",
+
+    2: "Normal (น้ำหนักปกติ)",
+
+    3: "Overweight (น้ำหนักเกิน)",
+
+    4: "Obesity (โรคอ้วน)",
+
+    5: "Extreme Obesity (โรคอ้วนอันตราย)"
+
 }
-
-engine_map = {
-    '800': 0,
-    '1000': 1,
-    '1200': 2,
-   '1500': 3,
-    '1800': 4,
-    '2000': 5,
-    '2500': 6,
-    '3000': 7,
-    '4000': 8,
-    '5000': 9
-}
-
-brand_map = {
-    'BMW': 0,
-    'Chevrolet': 1,
-    'Ford': 2,
-    'Honda': 3,
-    'Hyundai': 4,
-    'Kia': 5,
-    'Nissan': 6,
-    'Tesla': 7,
-    'Toyota': 8,
-    'Volkswagen': 9
-}
-
-transmission_map = {
-    'Automatic': 0,
-    'Manual': 1
-}
+ 
 with st.sidebar:
-    selected = option_menu('Prediction',
-                           ['Ridingmower','Used_cars'])
 
-if selected== 'Ridingmower':
+    selected = option_menu('Prediction', ['Ridingmower', 'Used_cars', 'bmi'])
+ 
+# --- Riding Mower Section ---
+
+if selected == 'Ridingmower':
+
     st.title('Riding Mower Classification')
-    
-    Income = st.text_input('Income')
-    LotSize = st.text_input('LotSize')
-    Riding_prediction = ''
-    if st.button('Predict'):
-        Riding_prediction = riding_model.predict([[
-            float(Income),
-            float(LotSize)
-            ]])
-        if Riding_prediction[0]==1:
-            Riding_prediction = 'Owner'
-        else:
-            Riding_prediction = 'Non Owner'
-    st.success(Riding_prediction)
-    
-if selected == 'Used_cars':
-    st.title('ประเมินราคารถมือ 2')
-    make_year = st.text_input('ปีที่ผลิต')
-    mileage_kmpl = st.text_input('กินน้ำมันกี่ KM/L')
-    engine_cc = st.selectbox('ขนาดเครื่องยนต์ (CC)', engine_map)
-    fuel_type = st.selectbox('ประเภทน้ำมัน', fuel_map)
-    owner_count = st.text_input('จำนวนเจ้าของเดิม')
-    brand = st.selectbox('ยี่ห้อรถ', brand_map)
-    transmission = st.selectbox('ประเภทเกียร์', transmission_map)
-    accidents_reported = st.text_input('จำนวนอุบัติเหตุที่เคยเกิด')
-    Price_predict = ''
-    if st.button('Predict'):
-        Price_predict = used_car_model.predict([[
-            float(make_year),
-            float(mileage_kmpl),
-            engine_map[engine_cc],
-            fuel_map[fuel_type],
-            float(owner_count),
-            brand_map[brand],
-            transmission_map[transmission],
-            float(accidents_reported)
-            ]])
-        Price_predict = round(Price_predict[0],2)
 
-    st.success(Price_predict)
+    Income = st.text_input('รายได้ (Income)')
+
+    LotSize = st.text_input('ขนาดพื้นที่ (LotSize)')
+
+    if st.button('ทำนายผล'):
+
+        prediction = riding_model.predict([[float(Income), float(LotSize)]])
+
+        res_text = 'เป็นเจ้าของ (Owner)' if prediction[0] == 1 else 'ไม่ได้เป็นเจ้าของ (Non Owner)'
+
+        st.success(f'ผลลัพธ์: {res_text}')
+ 
+# --- Used Cars Section ---
+
+if selected == 'Used_cars':
+
+    st.title('ประเมินราคารถมือ 2')
+
+    make_year = st.text_input('ปีที่ผลิต')
+
+    mileage_kmpl = st.text_input('อัตราสิ้นเปลือง (KM/L)')
+
+    engine_cc = st.selectbox('ขนาดเครื่องยนต์ (CC)', list(engine_map.keys()))
+
+    fuel_type = st.selectbox('ประเภทน้ำมัน', list(fuel_map.keys()))
+
+    owner_count = st.text_input('จำนวนเจ้าของเดิม')
+
+    brand = st.selectbox('ยี่ห้อรถ', list(brand_map.keys()))
+
+    transmission = st.selectbox('ประเภทเกียร์', list(transmission_map.keys()))
+
+    accidents_reported = st.text_input('จำนวนอุบัติเหตุ')
+ 
+    if st.button('ประเมินราคา'):
+
+        price = used_car_model.predict([[
+
+            float(make_year), float(mileage_kmpl), engine_map[engine_cc],
+
+            fuel_map[fuel_type], float(owner_count), brand_map[brand],
+
+            transmission_map[transmission], float(accidents_reported)
+
+        ]])
+
+        st.success(f'ราคาประเมินคือ: {round(price[0], 2)} บาท')
+ 
+# --- BMI Section ---
+
+if selected == 'bmi':
+
+    st.title('BMI Prediction')
+
+    gender = st.selectbox('เลือกเพศ', list(gender_map.keys()))
+
+    height = st.text_input('ส่วนสูง (cm)')
+
+    weight = st.text_input('น้ำหนัก (kg)')
+ 
+    if st.button('ทำนายผล BMI'):
+
+        # ส่งค่าตามลำดับที่ Model ต้องการ (ตรวจสอบ Gender, Height, Weight)
+
+        prediction = bmi_model.predict([[
+
+            gender_map[gender], 
+
+            float(height), 
+
+            float(weight)
+
+        ]])
+
+        # ดึงคำอธิบายจาก Index ที่ Model ทำนายออกมา
+
+        result_index = int(prediction[0])
+
+        description = bmi_index_details.get(result_index, "ไม่ทราบผลลัพธ์")
+
+        st.success(f'ผลการวิเคราะห์: {description}')
+ 
+ 
